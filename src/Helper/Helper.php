928c9ed1c8b3bbb222833a58a8e75442ece48398
Péter Balogh <peter.balogh@theory9.hu>
2015-04-06 19:56:05 +0200

    helper class

---
+++ src/Helper/Helper.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Shopreview\Helper;
+
+class Helper
+{
+    /**
+    * Check register globals and remove them
+    */
+    public static function unregisterGlobals() {
+        if (ini_get('register_globals')) {
+            $array = array(
+                '_SESSION', 
+                '_POST', 
+                '_GET', 
+                '_COOKIE', 
+                '_REQUEST', 
+                '_SERVER', 
+                '_ENV', 
+                '_FILES'
+            );
+            foreach ($array as $value) {
+                foreach ($GLOBALS[$value] as $key => $var) {
+                    if ($var === $GLOBALS[$key]) {
+                        unset($GLOBALS[$key]);
+                    }
+                }
+            }
+        }
+    }  
+    
+    /** 
+    * Check for Magic Quotes and remove them 
+    */
+    public static function stripSlashesDeep($value) {
+        $value = is_array($value) 
+            ? array_map(self::stripSlashesDeep, $value) 
+            : stripslashes($value);
+        
+        return $value;
+    }
+    /**
+    * Check for Magic Quotes and remove them
+    */
+    public static function removeMagicQuotes() {
+        if (get_magic_quotes_gpc() ) {
+            if(isset($_GET)){
+                $_GET = self::stripSlashesDeep($_GET);
+            }
+            
+            if(isset($_POST)){
+                $_POST = self::stripSlashesDeep($_POST);
+            }
+            
+            if(isset($_COOKIE)){
+                $_COOKIE = self::stripSlashesDeep($_COOKIE);
+            }
+            
+        }
+    }
+}
