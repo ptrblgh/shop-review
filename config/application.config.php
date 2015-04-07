0109df8491607de6fd936f3a5494a1f69fa96e8e
Péter Balogh <peter.balogh@theory9.hu>
2015-04-08 00:24:27 +0200

    init db adapter

--- config/application.config.php
+++ config/application.config.php
@@ -3,4 +3,10 @@
 return array(
     // application-wide config comes here
     'template_path' => 'view',
+    'db' => array(
+        'server' => 'localhost',
+        'username' => 'shop-review',
+        'password' => 'test',
+        'dbname' => 'shop-review'
+    )
 );
