e8d6dbe344cff7b0ae775aad380d8e78966af8af
Péter Balogh <peter.balogh@theory9.hu>
2015-04-08 15:16:47 +0200

    add array copy function to models

--- src/Mvc/Model/User.php
+++ src/Mvc/Model/User.php
@@ -7,4 +7,14 @@ class User
     public $username;
     public $password;
     public $email;
+
+    /**
+     * Retriving entity properties
+     * 
+     * @return array
+     */
+    public function getArrayCopy()
+    {
+        return get_object_vars($this);
+    }
 }
