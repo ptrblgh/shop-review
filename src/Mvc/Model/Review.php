7bfec5d3c1f0524ac9f8720a1bc0e44ed16c9eb5
Péter Balogh <peter.balogh@theory9.hu>
2015-04-08 15:16:47 +0200

    add array copy function to models

--- src/Mvc/Model/Review.php
+++ src/Mvc/Model/Review.php
@@ -8,4 +8,14 @@ class Review
     public $user_id;
     public $review_text;
     public $erview_rating;
+
+    /**
+     * Retriving entity properties
+     * 
+     * @return array
+     */
+    public function getArrayCopy()
+    {
+        return get_object_vars($this);
+    }
 }
